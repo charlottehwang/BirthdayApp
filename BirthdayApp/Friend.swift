fe60025ad41121cf0c1375e0b8baff898e29a36b
Charlotte <charlotteehwang@gmail.com>
2025-07-25 10:32:02 -0700

    Added Friend Class and UI to Add Friend

---
+++ BirthdayApp/Friend.swift
@@ -0,0 +1,17 @@
+//
+//  Friend.swift
+//  BirthdayApp
+//
+//  Created by Charlotte Hwang on 7/25/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
