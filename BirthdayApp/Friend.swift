9456002352ec61fcf2bed248dec54144a1ceb00f
Charlotte <charlotteehwang@gmail.com>
2025-07-25 11:19:27 -0700

    SwiftData

--- BirthdayApp/Friend.swift
+++ BirthdayApp/Friend.swift
@@ -6,10 +6,13 @@
 //
 
 import Foundation
+import SwiftData
 
+@Model
 class Friend {
     var name: String
     var birthday: Date
+    
     init(name: String, birthday: Date){
         self.name = name
         self.birthday = birthday
